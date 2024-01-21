bdf9c3a6e6613e48d1f543046a4192e82e7ede24
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-21 07:37:01 -0600

    progress

--- bun-sse/src/server.ts
+++ bun-sse/src/server.ts
@@ -1,36 +1,36 @@
-import { Hono } from "hono";
-import type { Context } from "hono";
-import { serveStatic } from "hono/bun";
-import { streamSSE } from "hono/streaming";
+import {Hono} from 'hono';
+import type {Context} from 'hono';
+import {serveStatic} from 'hono/bun';
+import {streamSSE} from 'hono/streaming';
 
 const app = new Hono();
-app.use("/*", serveStatic({ root: "./public" }));
+app.use('/*', serveStatic({root: './public'}));
 
-app.get("/greet", (c: Context) => {
-  return c.text("Hello Bun!");
+app.get('/greet', (c: Context) => {
+  return c.text('Hello Bun!');
 });
 
-app.get("/sse", (c: Context) => {
-  return streamSSE(c, async (stream) => {
+app.get('/sse', (c: Context) => {
+  return streamSSE(c, async stream => {
     // This should be invoked when the client calls close on the EventSource,
     // but it is not!
     // TODO: See https://github.com/honojs/hono/issues/1770.
-    console.log("calling onAbort");
+    console.log('calling onAbort');
     stream.onAbort(() => {
       // TODO: Why is this never called?
-      console.log("aborted");
+      console.log('aborted');
     });
 
-    await stream.writeSSE({ data: "starting" });
+    await stream.writeSSE({data: 'starting'});
 
     let count = 0;
     while (count < 10) {
       count++;
 
       await stream.writeSSE({
-        event: "count",
+        event: 'count',
         id: String(crypto.randomUUID()),
-        data: String(count),
+        data: String(count)
       });
     }
   });
